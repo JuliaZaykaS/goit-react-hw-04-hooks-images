a766e2ad449cbfae59503a947d388880b426341a
JuliaZaykaS <sys55@mail.ru>
2021-08-20 10:22:46 +0300

    модалка на хуках

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,37 +1,82 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
 const modal = document.querySelector('#modal-root');
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscClick);
-  }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscClick);
-  }
+export default function Modal({onClose, clearModal, children}) {
+  useEffect(() => {
+    window.addEventListener('keydown', onEscClick);
+    return () => {
+      window.removeEventListener('keydown', onEscClick);
+    }
+  })
 
-  onEscClick = e => {
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.onEscClick);
+  // }
+
+  // componentWillUnmount() {
+  //   window.removeEventListener('keydown', this.onEscClick);
+  // }
+
+  const onEscClick = e => {
     if (e.code === 'Escape') {
       this.props.onClose();
       this.props.clearModal();
     }
   };
 
-  onBackdropClick = e => {
+  const onBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
-      this.props.clearModal();
+      onClose();
+      clearModal();
     }
   };
 
-  render() {
+  // render() {
     return createPortal(
-      <div className={s.Overlay} onClick={this.onBackdropClick}>
-        <div className={s.Modal}>{this.props.children}</div>
+      <div className={s.Overlay} onClick={onBackdropClick}>
+        <div className={s.Modal}>{children}</div>
       </div>,
       modal,
     );
-  }
+  // }
 }
+// import { Component } from 'react';
+// import { createPortal } from 'react-dom';
+// import s from './Modal.module.css';
+
+// const modal = document.querySelector('#modal-root');
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onEscClick);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onEscClick);
+//   }
+
+//   onEscClick = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//       this.props.clearModal();
+//     }
+//   };
+
+//   onBackdropClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//       this.props.clearModal();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={s.Overlay} onClick={this.onBackdropClick}>
+//         <div className={s.Modal}>{this.props.children}</div>
+//       </div>,
+//       modal,
+//     );
+//   }
+// }
